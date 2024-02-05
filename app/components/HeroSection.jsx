975ad30f1bc98682925d2c91159eaba5c83cfea0
ramisalah2002 <ramisalah2002@gmail.com>
2024-02-05 23:47:28 +0100

    updating navbar

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
                 <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                     <Image
-                        src='/images/dev1.png'
+                        src='/images/dev2.png'
                         className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
                         alt='dev image'
                         width={300}
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,8 @@ import React, {useState} from 'react'
 import Link from 'next/link'
 import NavLink from './NavLink'
 import {Bars3Icon, XIcon, XMarkIcon} from "@heroicons/react/24/solid"
+import Image from 'next/image';
+import MenuOverlay from './MenuOverlay';
 
 const navLinks = [
   {
@@ -25,7 +27,12 @@ const Navbar = () => {
     <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
         <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-2'>
             <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
-                LOGO
+            <Image
+                        src='/images/logo.png'
+                        alt='dev image'
+                        width={150}
+                        height={150}
+                    />
             </Link>
             <div className='mobile-menu block md:hidden'>
               {
@@ -51,8 +58,10 @@ const Navbar = () => {
                   }        
                 </ul>
             </div>
-            
         </div>
+        {
+          navbarOpen ? <MenuOverlay links={navLinks}/> : null
+        }
     </nav>
   )
 }
